e88ffcab1a46517816a1ae12c2c019aaeb3f249a
jeesay <jeanchristophe.taveau@gmail.com>
2021-03-31 16:40:46 +0200

    Update widgetFactory.js

--- src/gui/widgetFactory.js
+++ src/gui/widgetFactory.js
@@ -1,8 +1,8 @@
 /*
- *  TWIN: Tiny Web Image Nodes
- *  Copyright (C) 2019  Jean-Christophe Taveau.
+ *  TINNED: TINy Node EDitor
+ *  Copyright (C) 2021  Jean-Christophe Taveau.
  *
- *  This file is part of TWIN
+ *  This file is part of TINNED
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
