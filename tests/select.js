590e546975f201fa09d4ddb2713aea967901b181
Quentin Blechet <quentin.blechet@gmail.com>
2021-03-31 15:27:32 +0200

    Tests Unitaires + Fonctions Select

--- tests/select.js
+++ tests/select.js
@@ -4,9 +4,27 @@ function setflag(mol){
     }
 }
 
-function select_AA(mol,aa){
+function select(mol,key,value){
     for (let i = 0; i < mol.pdb.atoms.length; i++){
-        if (mol.pdb.atoms[i].resName === aa){
+        if (mol.pdb.atoms[i][key] === value){
+            mol.flag[i] = 1;
+        }
+    }
+    return mol;
+}
+
+function select_and(mol,mol1,mol2){
+    for (let i=0; i<mol1.length; i++){
+        if (mol1.flag[i] === 1 && mol2.flag === 1){
+            mol.flag[i] = 1;
+        }
+    }
+    return mol;
+}
+
+function select_or(mol,mol1,mol2){
+    for (let i=0; i<mol1.length; i++){
+        if (mol1.flag[i] === 1 || mol2.flag === 1){
             mol.flag[i] = 1;
         }
     }
--- tests/test.js
+++ tests/test.js
@@ -6,18 +6,55 @@ describe("Classification",()=>{
 
 describe("Selection Atome", function() {
     it("should transform flag to 1", function() {
-        select_AA(mol_1zni,"TYR");
-        expect(mol_1zni.flag[1]).toBe(1);
-        expect(mol_1zni.flag[2]).toBe(1);
+        let res = select(mol_1zni,"resName","TYR");
+        expect(res.flag[1]).toBe(1);
+        expect(res.flag[2]).toBe(1);
     });
 });
 
 describe("Selection Element", function(){
     it("should transform flag to 1", function() {
-        select_Elem(mol_1zni,"C");
-        expect(mol_1zni.flag[1]).toBe(1);
-        expect(mol_1zni.flag[2]).toBe(1);
-        expect(mol_1zni.flag[3]).toBe(1);
+        let res = select(mol_1zni,"element","C");
+        expect(res.flag[1]).toBe(1);
+        expect(res.flag[2]).toBe(1);
+        expect(res.flag[3]).toBe(1);
     });
 });
 
+describe("Selection Numero", function(){
+    it("should transform flag to 1", function() {
+        let res = select(mol_1zni,"serial",92);
+        expect(res.flag[1]).toBe(1);
+    });
+});
+
+describe("Atome d'azote ou Tyrosine", function(){
+    it("should transform flag to 1", function() {
+        let res = select_or(mol_1zni,select(mol_1zni,"name","N"),select(mol_1zni,"resName","TYR"))
+        expect(res.flag[0]).toBe(1);
+        expect(res.flag[1]).toBe(1);
+        expect(res.flag[2]).toBe(1);
+    });
+});
+
+describe("Atome de carbone et Tyrosine", function(){
+    it("should transform flag to 1", function() {
+        let res = select_and(mol_1zni,select(mol_1zni,"element","C"),select(mol_1zni,"resName","TYR"));
+        expect(res.flag[1]).toBe(1);
+        expect(res.flag[2]).toBe(1);
+    });
+});
+
+describe("Atome de carbone, Tyrosine et Carbone Alpha", function(){
+    it("should transform flag to 1", function() {
+        let res = select_and(mol_1zni,select_and(mol_1zni,select(mol_1zni,"element","C"),select(mol_1zni,"name","CA")),select(mol_1zni,"resName","TYR"));
+        expect(res.flag[1]).toBe(1);
+    });
+});
+
+describe("Atome de carbone, Tyrosine ou Carbone Alpha", function(){
+    it("should transform flag to 1", function() {
+        let res = select_or(mol_1zni,select_or(mol_1zni,select(mol_1zni,"element","C"),select(mol_1zni,"name","CA")),select(mol_1zni,"resName","TYR"));
+        expect(res.flag[1]).toBe(1);
+    });
+});
