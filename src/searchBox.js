8186a96786fac5263b08455764f892d0d39bead0
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2021-04-26 16:23:01 +0200

    Update search

--- src/searchBox.js
+++ src/searchBox.js
@@ -24,29 +24,111 @@
 
 'use strict';
 
+import {TINNED} from './tinned.js';
+import * as DOM from './dom/dom.js';
+
 /**
  * @param {dom} element - HTML parent element
  * @param {array} titles - [{id:<string>,description: <string>,tags: [<string>, ...]},...]
  */
 export const searchBox = (parent,tags) => {
 
-  const displayNode = (tags,id) => {
-    const htmlString = tags
-        .map((tag) => {
+  let searchbox;
+  let posX;
+  let posY;
+
+  const displayNode = (parent,_tags) => {
+
+    // New node
+    const newNode = (e) => {
+      // Step #1 - Add new node
+      TINNED.graph.appendNode(t.id,-1,{meta: {pos:[posX,posY]},state: {}});
+      // Step #2 - Add new eventlistener for drawing edge between sockets
+      // Step #3 - Close searchbox
+      searchbox.display.style = 'none';
+    }
+
+
+    // MAIN
+    parent.innerHTML = '';
+    _tags.forEach((t) => {
+      let el = DOM.h('li.filtered', {},
+      [
+        DOM.h('a',
+        {
+          attrs: {
+            href:'#'
+          },
+          on: {
+            click: newNode
+          },
+          dataset: {
+            nodeid: t.id
+          }
+        },
+        t.description)
+      ]);
+      parent.appendChild(el);
+    });
+/*
             return `
             <li class="Node">
                 <h2><a href=# onclick=return board.graph.appendNode(${tag.id});>${tag.description}</a></h2>
             </li>
         `;
-        })
-        .join('');
-    id.innerHTML = htmlString;
+       */ 
+
   };
 
   const displaySearchbox = () => {
-    const searchbox = document.createElement('div');
-    searchbox.className = "searchbox"; 
-    const html = `<div class="header search"> 
+
+    const searchNodes = (e) => {
+      const nodeList = document.getElementById("nodeList");
+      const searchString = e.target.value.toLowerCase();
+      const filteredTags = tags.filter((tag) => {
+          return (
+              tag.tags.join(",").includes(searchString) ||
+              tag.description.toLowerCase().includes(searchString) ||
+              tag.class.toLowerCase().includes(searchString)
+          );
+        });
+      const filteredTagsV2 = filteredTags.slice(0, 5);
+      displayNode(nodeList,filteredTagsV2);
+    }
+
+    searchbox = document.createElement('div');
+    searchbox.className = "searchbox";
+    searchbox.style.display = 'none';
+    searchbox.style.position = 'absolute';    
+    searchbox.addEventListener('click',(e) => e.stopPropagation());
+    let search = DOM.input('#searchbar',
+    {
+      attrs: { 
+        type: "text",
+        name: 'searchbar',
+        placeholder: "search for a Node",
+        value: 0
+      },
+      on: {keyup: searchNodes}
+    },
+    []);
+    let ulist = DOM.h('ul#nodeList',{},[]);
+
+    searchbox.appendChild(search);
+    searchbox.appendChild(ulist);
+
+    /*
+    let searchbox = DOM.div(
+      '.header.search',
+      {},
+      [
+        p('Searchbox'),
+        div(.'body',[
+          div('#searchWrapper',
+          [search])
+        ])
+      ]
+    );
           <p> Searchbox </p>
         </div>
         <div class = "body">
@@ -64,28 +146,22 @@ export const searchBox = (parent,tags) => {
           <a href=# >fermer la searchbox</a>
         </div>`;
       searchbox.innerHTML = html;
+      */
+
     return searchbox;
   };
 
-  // **WARNING**. Check if there is no other event in Board!!
-  // Add Events
-  parent.onmouseup = function (){
-    const NodeList = document.getElementById("NodeList");
-    const searchbar = document.getElementById("searchbar");
-    searchbar.addEventListener('keyup', (e) => {
-      const searchString = e.target.value.toLowerCase();
-      const filteredTags = tags.filter((tag) => {
-          return (
-              tag.tags.join(",").includes(searchString) ||
-              tag.description.toLowerCase().includes(searchString) ||
-              tag.class.toLowerCase().includes(searchString)
-          );
-        });
-      const filteredTagsV2 = filteredTags.slice(0, 5);
-      displayNode(filteredTagsV2,NodeList);
-    });
-  };
+  const toggleDisplay = (ev) => {
+    console.log(ev);
+    posX = ev.clientX;
+    posY = ev.clientY;
+    searchbox.style.top = `${ev.clientY}px`;
+    searchbox.style.left = `${ev.clientX}px`;
+    searchbox.style.display = (searchbox.style.display === 'none') ? 'block' : 'none';
+    return false;
+  }
 
+  /*
   const Hide = (classe) => {
     document.getElementsByClassName(classe).style.display = "none";
   }
@@ -101,7 +177,26 @@ export const searchBox = (parent,tags) => {
       parent.onmousedown(Hide("searchbox"));
     }
   }
+*/
+
+/*
+const searchbar = document.getElementById("searchbar");
+searchbar.addEventListener('keyup', (e) => {
+  const NodeList = document.getElementById("NodeList");
+  const searchString = e.target.value.toLowerCase();
+  const filteredTags = tags.filter((tag) => {
+      return (
+          tag.tags.join(",").includes(searchString) ||
+          tag.description.toLowerCase().includes(searchString) ||
+          tag.class.toLowerCase().includes(searchString)
+      );
+    });
+  const filteredTagsV2 = filteredTags.slice(0, 5);
+  displayNode(filteredTagsV2,NodeList);
+});
+*/
 
   parent.appendChild(displaySearchbox());
-  check_mouse_down("searchbox");
+  parent.addEventListener('click',toggleDisplay);
+  // check_mouse_down("searchbox");
 }
