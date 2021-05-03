1098286265e85a508d79a4feeaeb4637209cf3d5
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2021-05-03 17:32:42 +0200

    Update fetchmol

--- components/mol/fetchmol.js
+++ components/mol/fetchmol.js
@@ -24,91 +24,41 @@
 
 'use strict';
 
-import {Observable} from '../../src/functional/observable.js';
 
-// Simplified parser
-const parsePDB = (txt) => {
-  function parseHeader(row) {
-    // Parsing Data from wwwPDB
-    const tokens = [
-      {start:11,end:50,field: 'classification', type: 'string'},
-      {start:51,end:59,field: 'depDate', type: 'date'},
-      {start:63,end:66,field: 'idCode', type: 'IDcode'}
-    ];
-    // Main Loop
-    let outputs = {};
-    for (let i = 0; i < tokens.length; i++) {
-      let value = row.slice(tokens[i].start - 1, tokens[i].end).trim();
-      if (tokens[i].type === 'number') {
-        value = parseFloat(value);
-      }
-      outputs[tokens[i].field] = value;
+import { fromIter } from '../../callbags/callbag-from-iter.js';
+import { map } from '../../callbags/callbag-map.js';
+import { pipe } from '../../callbags/callbag-pipe.js';
+import {parsePDB} from './io/parsePDB.js';
+
+const parse = (txt)  => {
+  console.log(txt);
+  const mol = parsePDB(txt);
+  return (start, sink) => {
+    if (start !== 0) return;
+
+    let copy = mol.slice();
+    let disposed = false;
+
+    sink(0, type => {
+      if (type !== 2) return;
+      disposed = true;
+      copy.length = 0;
+    })
+
+    while (copy.length !== 0) {
+      sink(1, copy.shift());
     }
-    return outputs;
+
+    if (disposed) return;
+
+    sink(2);
   }
-  
-  function parseAtom(row) {
-    // Parsing data from wwwPDB
-    let type = row.slice(0,6).trim();
-    let serial = parseInt(row.slice(7-1,11).trim() );
-    let name = row.slice(13-1,16).trim(); 
-    let resName = row.slice(18-1 , 20).trim();
-    let chainID = row.slice(22-1 , 23).trim();
-    let resSeq = parseInt(row.slice(23-1 , 26).trim() );
-    let iCode = row.slice(27-1, 28).trim();
-    let x = parseFloat(row.slice(31-1, 38).trim() );
-    let y = parseFloat(row.slice(39-1,46).trim() );
-    let z = parseFloat(row.slice(47-1,54).trim() );
-    let occupancy = row.slice(55-1 ,60).trim();
-    let tempFactor= row.slice(61-1 ,66).trim();
-    let element = row.slice(77-1 ,78).trim();
-    let charge= row.slice(79-1 , 80).trim();
-    let color = [255,128,50]; // TODO toGLcolor(COLORS.cpk[element]);
-    
-    let title = chainID + '.'+ resName + '[' + resSeq + '].' + name + '[' + serial + '].' + element;
-    return {type,serial,title,name,resName,chainID,resSeq,x,y,z,element, color};
-  }
-
-  // MAIN
-  const rows = txt.split('\n');
-  let atoms = [
-    {
-      type: 'META',
-      serial:0,
-      name: 'Head', 
-      resName: '_',
-      chainID: '_',
-      resSeq: 0,
-      iCode: '_',
-      x: 0,
-      y: 0,
-      z: 0,
-      occupancy: 0,
-      tempFactor: 0,
-      element: '_',
-      charge: 0,
-      content: []
-    }
-  ];
-  rows.forEach( row => {
-    let type = row.slice(0,6).trim();
-    if (type === 'ATOM' || type === 'HETATM') {
-      atoms.push(parseAtom(row));
-    }
-    else if (type === 'HEADER') {
-      atoms[0].content.push(parseHeader(row));
-    }
-  });
-  return atoms;
-}
-
+} 
 const fetchmol = (node) => (stream) => {
-  // Params
-  const PATH = 'https://files.rcsb.org/view/';
-  let pdbid = node.data.state?.pdbid;
-  let url = `${PATH}${pdbid.toUpperCase()}.pdb`;
 
-  const obs = new Observable(observer => {
+  const done = () => console.log('Done');
+
+  const fetchPDB = fromPromise(
     fetch(url)
     .then( response => {
       if (response.ok) {
@@ -119,21 +69,82 @@ const fetchmol = (node) => (stream) => {
         console.log('Wait..');
       }
     })
-    .then( txt => {
-      let mol = parsePDB(txt);
-      for (let atom of mol) {
-        observer.next(atom);
-      }
-      observer.complete();
+    .then(txt => {
+      const atoms = txt.split('\n').filter(row => row.slice(0,6).trim() === 'ATOM' ||  row.slice(0,6).trim() === 'HETATM');
+      length = 20;
+      return atoms.slice(1,20);
     })
-    .catch( err => observer.error(err));
-    return () => {
-      // Do nothing for unsubscribe
-    }
-  });
+    .catch( err => console.error(err) )
+  );
+  
+  pipe(
+    fetchPDB,
+    switchMap(atoms => fromIter(Array.from({length: atoms.length}, (_,i) => i)), (atoms,num) => ({num,a:atoms[num]}) ),
+    forEach(x => console.log(x))
+  );
 
+  
+
+
+  const fetchPDB = (_url) => {
+    // Init
+    let atoms = [];
+    // Fetch data
+    fetch(_url)
+    .then( response => {
+      if (response.ok) {
+        console.log('Download Done...');
+        return response.text();
+      }
+      else {
+        console.log('Wait..');
+      }
+    })
+    .then(txt => {
+      return Promise.resolve(parsePDB(txt));
+    })
+    .then(data => {
+      atoms = data.slice(0,20);
+      console.log(atoms);
+      return atoms;
+    })
+    .catch( err => console.error(err)) // TODO
+
+    return (start,sink) => {
+      if (start !== 0) {
+        return;
+      }
+      let count = 0;
+      let completed = false;
+      sink(0,t => {
+        console.log('count',count);
+        if (completed) return;    
+        if (t === 1) {
+          // Loop        
+          if (count === atoms.length) {
+            sink(2);
+            completed = true;
+          }
+          else {
+            // Next
+            console.log('next');
+            sink(1, atoms[count++]);
+          }  
+          sink(2);
+        } else if (t === 2) {
+          completed = true;
+          sink(2);
+        }
+      });
+    };
+  };
+        
+  // Params
+  const PATH = 'https://files.rcsb.org/view/';
+  let pdbid = node.data.state?.pdbid;
+  let url = `${PATH}${pdbid.toUpperCase()}.pdb`;
   // Set observable in stream
-  node.targets.forEach( key => stream[key] = obs);
+  node.targets.forEach( key => stream[key] = fetchPDB(url));
   // Return stream
   return stream;
 }
