f1dcee7bcc8aeb077afaec0b69f4439101c76ab5
jeesay <jeanchristophe.taveau@gmail.com>
2021-05-11 15:19:52 +0200

    Create iterable.js

---
+++ doc/basics/iterable.js
@@ -0,0 +1,44 @@
+# Iterable
+
+_Stream an iterable primitive/object (String, Array, etc.)_
+
+| Screenshot of the node |
+|------------------------|
+|Legend|
+
+
+## Inputs
+
+### None
+
+_None_
+
+## Properties
+   
+### Property Name
+
+_Description_
+
+## Outputs
+
+### Output Name
+    _A stream of items_
+
+## Example
+
+### String
+
+### Array
+
+### Other Data Structures
+
+This is only for expert programmer because we have to wrap the data into a IIFE function.
+
+For example, a Map object may be defined as follows.
+
+```javascript
+(()=>{let map = new Map();
+map.set('a',100);
+return map;
+})()
+```
