c192048bf348247c262e52dc2012068c5516a5fe
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2021-06-02 10:36:38 +0200

    First release

---
+++ callbags/callbag-repeat.js
@@ -0,0 +1,36 @@
+export const repeat = (repititions = -1) => source => (start, sink) => {
+  if (start !== 0) return;
+  let count = repititions;
+  let didInit = false;
+
+  let sourceTalkback;
+  function sinkTalkback(type, data) {
+    sourceTalkback(type, data);
+  }
+
+  const subscribe = () => {
+    source(0, (t, d) => {
+      if (t === 0) {
+        sourceTalkback = d;
+        if (didInit) {
+          sourceTalkback(1);
+          return;
+        }
+        didInit = true;
+        sink(0, sinkTalkback);
+        return;
+      }
+
+      if (t === 2 && !d && count !== 0) {
+        count--;
+        subscribe();
+        return;
+      }
+
+      sink(t, d);
+    });
+  };
+  subscribe();
+};
+
+// module.exports = repeat;
